e3f7371dda01651896010c52e4a0176502372cd1
ajeigbepaul <pdave4krist@yahoo.com>
2024-10-24 20:01:15 +0100

    resolving build error

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import BannerInfo from "./BannerInfo";
-import { useState } from "react";
+// import { useState } from "react";
 
 function Slider({
   image,
